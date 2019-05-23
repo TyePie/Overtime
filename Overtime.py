9f19c91d8a94aa6a04f51981602bcad0115fe899
Tyrone <tyroneanderson0@gmail.com>
2019-05-23 12:36:46 +0200

    array and csv working
    Added numerous features in order to gather information into an array and write to a csv file.

--- Overtime.py
+++ Overtime.py
@@ -1,3 +1,6 @@
+import sys
+import csv
+
 print('Overtime App by Tyrone')
 
 fname = input('Please insert your first name: ')
@@ -12,11 +15,11 @@ while len(fname) == 0:
 lname = input('Please insert your last name: ')
 
 print('Thank you ', fname, ' ', lname, ',')
-ticknos = input('please insert the ticket numbers you worked on, seperated by a comma: ')
+ticknos = input('please insert the ticket numbers you worked on, seperated by a hyphen(-): ')
 
 print('Thank you, ', fname, ' ', lname, ', you have worked on tickets: ', ticknos)
 
-custname = input('Please insert the names of the customers who created these tickets, also seperated by a comma: ')
+custname = input('Please insert the names of the customers who created these tickets, also seperated by a hyphen: ')
 
 start = input('Please insert your start time as format hh:mm: ')
 
@@ -26,3 +29,21 @@ print('You have entered the following information:')
 print('You are: ', fname, ' ', lname)
 print('You worked on the following tickets in this session: ', ticknos)
 print('Your session started at: ', start,', and ended at: ', end)
+
+line = [[fname, lname, ticknos, custname, start, end]]
+
+decision = input('Can you confirm the above information is correct and it can be written to the file?(Y/N) ')
+
+f = open('output.csv', 'w')
+
+if decision == 'Y':
+	with f:
+		writer = csv.writer(f)
+		writer.writerows(line)
+		sys.exit('OT Logged.')
+elif decision == 'N':
+	print('Cancelling operation...')
+	sys.exit('Program closed.')
+
+else:
+	sys.exit('Incorrect confirmation provided. Closing.')
